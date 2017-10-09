771afbcea0aa1eebf0646591583099b9e51da70b
Arona SOW <asow@remade.com>
2017-10-09 16:16:32 +0200

    First commit Arona

--- app/src/main/java/com/example/amady/parcelprotect/Login.java
+++ app/src/main/java/com/example/amady/parcelprotect/Login.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -101,6 +102,9 @@ public class Login extends AppCompatActivity
                 startActivity(intent);
             }
         });
+
+        int i = 0;
+        Log.d("TAG","print i:" + i);
 
     }
 
